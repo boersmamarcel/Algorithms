4922104bb7390fab1872ce7f572baaefb02db895
Marcel Boersma <boersma.marcel@gmail.com>
2011-12-02 11:38:35 +0100

    Adds main file and main test file

---
+++ maximum-subarray-algorithm/main.c
@@ -0,0 +1,20 @@
+/******************************************************************************************************
+*
+*					Find the maximum subarray by Marcel Boersma 2/12/2012													
+*
+*					This algorithm will find the maximum subarray for a given array
+*
+******************************************************************************************************/
+
+
+#include <stdio.h>
+
+
+int main(int argc, char * argv[])
+{
+
+	
+	return 0;
+}
+
+
