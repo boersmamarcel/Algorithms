60e1a7fbf72a8befc87f75cd88185d6dc517eb36
Marcel Boersma <boersma.marcel@gmail.com>
2012-01-28 18:25:34 +0100

    Changes .gitignore to not include compiled sources and adds argument validation on insertion sort

--- insertion-sort/insertionsort.c
+++ insertion-sort/insertionsort.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
@@ -9,7 +10,35 @@ int main(int argc, char* argv[])
 		printf("\nWelcome to the insertion-sort algorithm example!\n");
 		printf("--------------------------------------------------\n\n");
 		printf("It works as follows:\n");
-		printf("You can let your array be sorted ascending.\nGive an array to insertion sort like this insertion-sort <4,2,6,7,2,4,9> and we will show you how we sort it.\n\n");
+		printf("You can let your array be sorted ascending.\nGive an array to insertion sort like this insertion-sort {4,2,6,7,2,4,9} ( 0 not allowed ) and we will show you how we sort it.\n\n");
+	}else if(argc > 1){
+		printf("\n Hi there! We will show you how insertion sort works... thx for watching.\n\n");
+		
+		bool valid_arguments = true;
+		int numbers[argc - 1];	
+		while(argc-- && valid_arguments == true)
+		{
+			if(argc > 0)
+			{
+				//all the arguments other than the running command must be a number in order to sort it, atoi returns 0 incase it isn't a numbers
+				if(atoi(argv[argc]) == 0)
+				{
+					printf("Whoops, you didn't use insertion sort correctly try to run the program again with the correct arguments.\n\n");
+					valid_arguments = false;
+				}else{
+					// insert the number on postion argc - 1 (we do not save argement 1 so the we have one argument less to save than argc counts
+					numbers[argc - 1] = atoi(argv[argc]);
+				}
+			}
+		}	
+		
+		if (valid_arguments == false){
+			return 1;//end the program with an error
+		}
+	
+		//no errors found we have an array with numbers to do insertion sort on so let's get started with sorting!!!!
+		//TODO: sort function call
+
 	}
 
 
