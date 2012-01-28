f2f9f2482426d69752fde69c32c3bf8ed2031ba8
Marcel Boersma <boersma.marcel@gmail.com>
2012-01-28 17:34:31 +0100

    Adds how to text to the example

---
+++ insertion-sort/insertionsort.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(int argc, char* argv[])
+{
+
+	if(argc == 1)
+	{
+		printf("\nWelcome to the insertion-sort algorithm example!\n");
+		printf("--------------------------------------------------\n\n");
+		printf("It works as follows:\n");
+		printf("You can let your array be sorted ascending.\nGive an array to insertion sort like this insertion-sort <4,2,6,7,2,4,9> and we will show you how we sort it.\n\n");
+	}
+
+
+	return 0;
+
+}
